5dd4bb78a0a55055d7d3d26f2ce52cc13ed8516f
Chris <csun@SURF-SUN.localdomain>
2020-11-15 16:55:14 -0500

    fade to black onclick

--- js/choosevis.js
+++ js/choosevis.js
@@ -25,12 +25,8 @@ class ChooseVis {
             .attr("height", vis.height)
             .attr("transform", "translate(" + vis.margin.left + ", " + vis.margin.top + ")");
 
-
-        vis.x = d3.scaleLinear()
-            .range([vis.margin.left, vis.width - vis.margin.right]);
-
         let randomX = d3.randomUniform(vis.margin.left * 5, vis.width - vis.margin.right * 5);
-        let randomY = d3.randomUniform(vis.margin.top * 5, vis.height - vis.margin.bottom * 5);
+        let randomY = d3.randomUniform(vis.margin.top * 8, vis.height - vis.margin.bottom * 5);
         let randomSize = d3.randomNormal(4, 17);
         let randomOpacity = d3.randomUniform(.4, .9);
         let randomTime= d3.randomUniform(0, 2000);
@@ -40,10 +36,6 @@ class ChooseVis {
         for (let i = 0; i < 70; i++) {
             circles.push([[randomX(), randomX(), randomX()], [randomY(), randomY(), randomY()], Math.abs(randomSize()),randomOpacity(), randomBinary()]);
         }
-        // circles = Array.from('0'.repeat(5))
-
-        let circleEnter = vis.svg.selectAll('circle')
-            .data(circles);
 
         let colors = ['dodgerblue', 'salmon', 'lightgreen', 'lemonchiffon'];
 
@@ -51,9 +43,20 @@ class ChooseVis {
         let reds = ['salmon', 'pink', 'indianred'];
         let greens = ['palegreen', 'lightgreen', 'darkseagreen'];
 
-        // colors = [blues, reds, greens];
+        let blue1 = ['#0390B8', '#62D4F4', '#04C8FF'];
+        let red1 = ['#FB5F9E', '#FF0067', '#E4005C'];
+        let yellow1 = ['#FFEA61', '#FFDD00', '#FFDD00'];
 
-        console.log(vis.companies);
+        // colors = [blue1, red1, yellow1];
+
+        let instructions = vis.svg.append('text')
+            .attr("text-anchor", "middle")
+            .attr('x', vis.width / 2)
+            .attr('y', vis.margin.top * 2);
+
+
+        let circleEnter = vis.svg.selectAll('circle')
+            .data(circles);
 
         circleEnter
             .enter()
@@ -63,29 +66,62 @@ class ChooseVis {
             .attr("r", d => d[2])
             .attr("opacity", 0)
             .attr("fill", (d, i) => colors[i%3])
+            // .attr("fill", (d, i) => colors[i%3][i%3])
+            .on("click", function(event, d){
+
+                let ok = vis.svg.selectAll('circle')
+                    .data(circles)
+
+                ok
+                    .enter()
+                    .merge(ok)
+                    .transition()
+                    .duration(3000)
+                    .attr("fill", function(d, i) {
+                        if (d[4] == 0) {
+                            return '#282828';
+                        }
+                        else {
+                            return colors[(i+2)%3];
+                        }
+                    })
+                    .attr("cy", function(d) {
+                        if (d[4] == 0) {
+                            return (vis.height + vis.margin.bottom * 16);
+                        }
+                        else {
+                            return (d[1][0]);
+                        }
+                    })
+
+                instructions
+                    .transition()
+                    .delay()
+                    .duration()
+                    .text('Click on a company!');
+
+            })
+
             .transition()
             .delay((d, i) => randomTime(i))
             .duration(3000)
             .attr("cx", d => d[0][1])
             .attr("cy", d => d[1][1])
-            .attr("r", d => d[2])
             .attr("opacity", d => d[3])
             .attr("fill", (d, i) => colors[i%3])
+            // .attr("fill", (d, i) => colors[i%3][i%3])
             .transition()
             .duration(2000)
             .attr("cx", d => d[0][2])
             .attr("cy", d => d[1][2])
-            .attr("r", d => d[2])
-            .attr("opacity", d => d[3])
             .attr("fill", (d, i) => colors[(i+1)%3])
+            // .attr("fill", (d, i) => colors[i%3][(i+1)%3])
             .transition()
             .duration(2000)
             .attr("cx", d => d[0][0])
             .attr("cy", d => d[1][0])
-            .attr("r", d => d[2])
-            .attr("opacity", d => d[3])
             .attr("fill", (d, i) => colors[(i+2)%3]);
-
+            // .attr("fill", (d, i) => colors[i%3][(i+2)%3]);
 
 
     }
